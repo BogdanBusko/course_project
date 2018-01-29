e73ccd7def127080ce4af5e70e507f15cffd07b3
BogdanBusko <busko.bogdan@gmail.com>
2018-01-29 22:05:45 +0200

    add seeds for courses

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,5 +1,15 @@
-category_count = Faker::Number.between(5, 10)
+categories_count = Faker::Number.between(5, 10)
 
-category_count.times do
-  Category.create!(name: "#{Faker::Educator.course}#{Faker::Number.number(3)}")
+categories_count.times do
+  Category.create!(name: Faker::Book.genre)
+end
+
+Category.each do |category|
+  Faker::Number.between(20, 50).times do
+    category.courses.create!(
+      name: Faker::Educator.course,
+      description: Faker::Lorem.paragraph,
+      start_date: Faker::Date.between(Date.today, Date.today + 366)
+    )
+  end
 end
