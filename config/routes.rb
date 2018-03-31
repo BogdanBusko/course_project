d2252b6456d26c7164cd37dee13b05d3be324d11
BogdanBusko <busko.bogdan@gmail.com>
2018-03-31 13:35:27 +0300

    modified default devise registration

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  devise_for :users
+  
   get 'category/show/:id', to: 'categories#show', as: 'category'
   get 'course/show/:id', to: 'courses#show', as: 'course'
 
